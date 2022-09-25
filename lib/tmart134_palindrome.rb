2505c16aaa7d82099fcc9331eb6abe262b784650
tmart134 <martinltracey@gmail.com>
2022-09-25 07:18:28 -0400

    Initialize repository

---
+++ lib/tmart134_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tmart134_palindrome/version"
+
+module Tmart134Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tmart134_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tmart134Palindrome
+  VERSION = "0.1.0"
+end
